e9d6d75fac3da7dada977c7336bde21f10ecdaa1
Umutml <118080093+Umutml@users.noreply.github.com>
2022-12-06 00:57:16 +0300

    v0.1
    Different ball layouts

--- Assets/Scripts/PickerCount.cs
+++ Assets/Scripts/PickerCount.cs
@@ -6,16 +6,21 @@ public class PickerCount : MonoBehaviour
 {
 
     static public int ballCount;
-    //[SerializeField]private int limitBall = 10; 
+    
     public Player player;
-    private int[] limits = { 3, 6, 7};
+    
     private int count = 0;
     private GameManager gamemanage;
+   
 
-    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        
+        
         count = 0;
         player = GameObject.Find("Magnet").GetComponent<Player>();
         gamemanage = GameObject.Find("GameManager").GetComponent<GameManager>();
@@ -43,7 +48,7 @@ public class PickerCount : MonoBehaviour
         if (other.CompareTag("Ball"))
         {
             ballCount++;
-            //Debug.Log(ballCount);
+            Debug.Log(ballCount);
            
         }
     }
@@ -54,27 +59,20 @@ public class PickerCount : MonoBehaviour
         
         yield return new WaitForSeconds(2f);
 
-        
-        
-        
-        if (ballCount >= gamemanage.limits[0])
+        if (ballCount >= gamemanage.limits[count])
         {
             player.isMoving = true;
             ballCount = 0;
             gamemanage.sayi++;
+            Debug.Log("Limit ve count" + gamemanage.limits[count]);
             count++;
-            Debug.Log("Limit ve count" + limits[count]);
-            
-            
-
             Debug.Log("Ballcount reseted");
-            
-            // limitball += 10;
+  
         }
         else
         {
             Debug.Log("Ball is not enough!");
-            Debug.Log("Limit ve count" + limits[count]);
+            Debug.Log("Limit ve count" + gamemanage.limits[count]);
         }
 
     }
